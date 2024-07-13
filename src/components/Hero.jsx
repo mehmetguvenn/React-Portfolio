5c4e1edbb4cea0566b499b94af2ee1f773674325
mehmet <mehmetguven519@gmail.com>
2024-07-13 20:07:28 +0300

    src updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,7 @@ const Hero = () => {
         <div className="w-full lg:w-1/2">
             <div className="flex flex-col items-center lg:items-start">
                 <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Mehmet Güven</h1>
+                <div className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Full Stack Developer</div>
             </div>
         </div>
     </div>

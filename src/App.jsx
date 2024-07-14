85e13a779247ddc30c82fe48721c703d0b0be181
mehmet <mehmetguven519@gmail.com>
2024-07-14 23:01:41 +0300

    portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import Hero from "./components/Hero"
-import Navbar from "./components/Navbar"
+import Contact from "./components/Contact";
+import Projects from "./components/Projects";
+import Experience from "./components/Experience";
+import Technologies from "./components/Technologies";
+import About from "./components/About";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className="overflow-x-hidden text-black antialiased selection:bg-white selection:text-cyan-900">
-      <div className="flixed top-0 -z-10 h-full w-full"></div>
+      <div className="fixed top-0 -z-10 h-full w-full">
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-      
+      </div>
+            
       <div className="container mx-auto px-8">
           <Navbar />
           <Hero />
+          <About />
+          <Technologies />
+          <Experience />
+          <Projects />
+          <Contact />
       </div>
     </div>
   )
--- src/constants/index.js
+++ src/constants/index.js
@@ -3,39 +3,43 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `Web alanın da bir çok çalışma yaptım ve bir çok full stack projler var. Web alanında uzmanlığa doğru gidiyorum bu sayfa yeni öğrenmiş olduğum reactjs ile kodlanmıştır.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `Verimli ve kullanıcı dostu web siteleri oluşturma tutkusuna sahip. 
+Kendimi web alanına adamış yıllardır bu alanla ilgili işler yapmış ve hala yapmaya devam ediyorum.Html, Css, Php, Js, Mysql, C# gibi çeşitli teknolojilerle çalıştım.
+Web geliştirme yolculuğum işlerin nasıl yürüdüğüne dair derin bir merakla başladı ve sürekli olarak öğrenmeye ve yeni zorluklara uyum sağlamaya çalıştığım bir kariyere dönüştü.
+Kodlamanın dışında aktif kalmaktan, yeni teknolojileri keşfetmekten ve açık kaynaklı projelere katkıda bulunmaktan keyif alıyorum.`;
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    year: "2024 - Present",
+    role: "Web Developer",
+    company: "Portfolio",
+    description: `Reactjs ile kodlanmış olduğum kendime ait bir portfolio sitesidir. React ile bugünün bir çok çalışma yapmayı ve her yaptığım çalışmayı burada paylaşmayı düşünüyorumç`,
+    technologies: ["Reactjs", "Html", "Javascript"],
   },
   {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
+    year: "2023 - 2024",
     role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+    company: "E-Ticaret",
+    description: `MySql ve Php kullanarak bir e-ticaret projesi geliştirdim. Web sitesi alanında ilk projelerimden bir tanesi oldu.`,
+    technologies: ["HTML", "CSS", "Php", "MySQL"],
   },
   {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+    year: "2023 - 2024",
+    role: "Full Stack Developer",
+    company: "Blog Sitesi",
+    description: `Mysql, Php, Html, Css, Javascript geliştirme ve sürdürme konusunda bir ekibe liderdlik ettim. Php ile kodladığımız projeyi mysql veri tabanıyla entegre edildi güzel bir iş çıkarıldı.`,
+    technologies: ["MySql", "Php", "Javascript"],
   },
+  {
+    year: "2023 - 2024",
+    role: "Full Stack Developer",
+    company: "E-ticaret",
+    description: `Visual Basic ve MySql bağlantılı ekip halinde yapılan bir proejede ki hataları çözdüm ve gerekli bakımları yaptım. Projenin tamamlanması için ekip ile çalışmalar yapıldı.`,
+    technologies: ["Visual Basic", "MySql"],
+  },
+
 ];
 
 export const PROJECTS = [
@@ -70,7 +74,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
+  address: "Kayseri",
+  phoneNo: "0555 555 5555",
   email: "me@example.com",
 };
